3c6ab917bade26664c5a13104768a0fec6f4fad0
sh601857 <yijiong.hu@gmail.com>
2016-06-24 17:28:07 +0800

    update

--- pysideTest.py
+++ pysideTest.py
@@ -7,59 +7,96 @@ from PySide import QtCore
 from PySide import QtGui
 
 class MainW(QtGui.QMainWindow):
-    
+
     def __init__(self):
         super(MainW, self).__init__()
-        
+
         self.initUI()
-        
+
     def initUI(self):               
         # create commonds tree        
-        model = QtGui.QStandardItemModel()
-        parentItem = model.invisibleRootItem()
-        for i in range(4):
-            item = QtGui.QStandardItem(self.tr("Step %d" % i) )
-            parentItem.appendRow(item)
-            for s in range(3):
-                cmditem = QtGui.QStandardItem("CMD %d" % s)
-                item.appendRow(cmditem)
-        cmdTree = QtGui.QTreeView(self)
-        cmdTree.setModel(model)
-        cmdTree.setHeaderHidden(True)
-        cmdTree.expandToDepth(2)
+        self.cmdmodel = QtGui.QStandardItemModel()
+        parentItem = self.cmdmodel.invisibleRootItem()
+        item = QtGui.QStandardItem(self.tr("1.Specification"))
+        item.appendRow(QtGui.QStandardItem("Simulation Specs"))
+        item.appendRow(QtGui.QStandardItem("CDU Variables"))
+        item.appendRow(QtGui.QStandardItem("Product Quality"))
+        item.appendRow(QtGui.QStandardItem("Monitored Variables"))
+        item.appendRow(QtGui.QStandardItem("HEN Stream Data"))
+        parentItem.appendRow(item)
+        
+        item = QtGui.QStandardItem(self.tr("2.Sampling"))
+        item.appendRow(QtGui.QStandardItem("Sample Plan"))
+        item.appendRow(QtGui.QStandardItem("Sampling"))
+        parentItem.appendRow(item)
+
+        item = QtGui.QStandardItem(self.tr("3.Analysis"))
+        item.appendRow(QtGui.QStandardItem("Settings"))
+        item.appendRow(QtGui.QStandardItem("Plots"))
+        item.appendRow(QtGui.QStandardItem("Rules"))  
+        item.appendRow(QtGui.QStandardItem("View Point"))      
+        parentItem.appendRow(item)
+        
+        item = QtGui.QStandardItem(self.tr("4.Build ANNS"))
+        item.appendRow(QtGui.QStandardItem("Settings"))
+        item.appendRow(QtGui.QStandardItem("Regress"))
+        item.appendRow(QtGui.QStandardItem("Plots"))       
+        parentItem.appendRow(item)      
+ 
+        item = QtGui.QStandardItem(self.tr("5.ANNS+i-Heat"))
+        item.appendRow(QtGui.QStandardItem("CDU Settings"))
+        item.appendRow(QtGui.QStandardItem("i-Heat Settings"))
+        item.appendRow(QtGui.QStandardItem("Simulate"))   
+        item.appendRow(QtGui.QStandardItem("Tables"))  
+        item.appendRow(QtGui.QStandardItem("Plots")) 
+        parentItem.appendRow(item)   
+                
+        self.cmdTree = QtGui.QTreeView(self)
+        self.cmdTree.setModel(self.cmdmodel)
+        self.cmdTree.setHeaderHidden(True)
+        self.cmdTree.expandToDepth(2)
         dockWidget = QtGui.QDockWidget((""), self)
         dockWidget.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea)
-        dockWidget.setWidget(cmdTree)
+        dockWidget.setWidget(self.cmdTree)
         dockWidget.setBaseSize(200,800)
         dockWidget.setTitleBarWidget(QtGui.QWidget(dockWidget))
         self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dockWidget)#hide the titlebar
-        
-	#Create central widget			
-        textEdit = QtGui.QTextEdit()		
-        self.setCentralWidget(textEdit)
-	
-	# Actions
+
+        #Create central widget			
+        self.firstPageWidget =  QtGui.QWidget()
+        self.secondPageWidget =  QtGui.QWidget()
+        self.thirdPageWidget =  QtGui.QWidget()
+
+        self.censw =  QtGui.QStackedWidget()
+        self.censw.addWidget(self.firstPageWidget)
+        self.censw.addWidget(self.secondPageWidget)
+        self.censw.addWidget(self.thirdPageWidget)	
+
+
+        self.setCentralWidget(self.censw)
+
+        # Actions
         exitAction = QtGui.QAction('Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(self.close)
 
         self.statusBar()
-	# menus
+        # menus
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAction)
 
         toolbar = self.addToolBar('Exit')
         toolbar.addAction(exitAction)
-        
+
         self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('Main window')    
+        self.setWindowTitle('CDU-Int')    
         self.showMaximized()
-        
-        
+
+
 def main():
-    
+
     app = QtGui.QApplication(sys.argv)
     ex = MainW()
     sys.exit(app.exec_())

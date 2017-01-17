b3366f639e4c162225f04d672eb8bbf229656e3c
sh601857 <yijiong.hu@gmail.com>
2017-01-17 17:54:29 +0800

    u

--- testexcel.py
+++ testexcel.py
@@ -1,12 +1,58 @@
 import time
+import pythoncom
 import win32com.client
 
+
+import threading
+stopEvent = threading.Event()
+
+
+class ExcelEvents:
+
+    _CanQuit = False
+    #def OnNewWorkbook(self, wb):
+        ##if type(wb) != types.InstanceType:
+            ##raise RuntimeError("The transformer doesnt appear to have translated this for us!")
+        #print ("OnNewWorkbook")
+    #def OnWindowActivate(self, wb, wn):
+        ##if type(wb) != types.InstanceType or type(wn) != types.InstanceType:
+            ##raise RuntimeError("The transformer doesnt appear to have translated this for us!")
+        #print("OnWindowActivate")
+    #def OnWindowDeactivate(self, wb, wn):
+        #print("OnWindowDeactivate")
+    #def OnSheetDeactivate(self, sh):
+        #print("OnSheetDeactivate")
+    #def OnSheetBeforeDoubleClick(self, Sh, Target, Cancel):
+        #print (Target.Column)
+        #return 0
+    def OnWorkbookBeforeClose(self, Wb, Cancel):
+        print ('OnWorkbookBeforeClose[{0}]'.format(Wb.FullName))
+        #if Wb.FullName == 'D:\\GitHub\\PySideSample\\test.xlsm':
+        self._CanQuit = True        
+        return 0
+    
+class WorkbookEvents:
+    _colsed = False
+    def OnActivate(self):
+        print ("workbook OnActivate")
+    def OnBeforeRightClick(self, Target, Cancel):
+        print ("OnBeforeRightClick")
+        
+    def onBeforeClose(self, Cancel):
+        print ("OnBeforeRightClick")
+
 # Open Excel
+#xlApp = win32com.client.DispatchWithEvents("Excel.Application", ExcelEvents)
+
+
+ExcelEvents._CanQuit = False
 xlApp = win32com.client.Dispatch("Excel.Application")
+xlApp = win32com.client.DispatchWithEvents(xlApp, ExcelEvents)
 
 # Show Excel. Unlike PPT, Word & Excel open up "hidden"
 xlApp.Visible = 1
-xlApp.UserControl = 0
+xlApp.UserControl = 1
+xlApp._CanQuit = False
 
 # Add a workbook
 wbTest = xlApp.Workbooks.Open('D:\\GitHub\\PySideSample\\test.xlsm')
@@ -26,12 +72,14 @@ wsSheet1.Cells(1, 2).Value = 0.0
 wsSheet1.Cells(1, 3).Value = 0.5
 wsSheet1.Cells(1, 4).Value = 1.0
 
+xlApp.Calculate()
 
-while xlApp.Visible:
+while xlApp._CanQuit == False:
 
     time.sleep(1)
+    pythoncom.PumpWaitingMessages()
 
-wbTest.Save()
+xlApp=None
 
 
 
